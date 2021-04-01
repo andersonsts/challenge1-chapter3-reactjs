cc5be50e6a9a741e7cef6d70e481609539cd639f
Anderson <andersonst.dev@gmail.com>
2021-03-31 23:04:58 -0300

    :rocket: feat: fetch data from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { GetStaticProps } from 'next';
-
 import Prismic from '@prismicio/client';
 import { useEffect } from 'react';
 import { getPrismicClient } from '../services/prismic';
@@ -26,24 +25,49 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
-  useEffect(() => {
-    const data = document.getElementById('root');
-  }, []);
-
-  return <h1>Home</h1>;
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  // useEffect(() => console.log('data', postsPagination), [postsPagination]);
+  return <h1>ok</h1>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  const postsResponse = await prismic.query(
-    [Prismic.predicates.at('document.type', 'posts')],
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
     {
-      pageSize: 50,
+      fetch: [
+        'post.uid',
+        'post.first_publication_date',
+        'post.title',
+        'post.subtitle',
+        'post.author',
+        'post.content',
+        'post.data',
+      ],
+      pageSize: 40,
     }
   );
 
+  const results = response.results.map(result => ({
+    uid: result.uid,
+    first_publication_date: result.first_publication_date,
+    data: {
+      title: result.data.title,
+      subtitle: result.data.subtitle,
+      author: result.data.author,
+    },
+  }));
+
+  const { next_page } = response;
+
+  const postsPagination = {
+    next_page,
+    results,
+  };
+
   return {
-    props: {},
+    props: {
+      postsPagination,
+    },
   };
 };
