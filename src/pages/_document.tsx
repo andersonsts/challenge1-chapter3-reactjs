1a85721b8cf672dfeab0e39ef8ab06cf403e5374
andersonsts <andersonst.dev@gmail.com>
2021-07-10 17:04:38 -0300

    init preview

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,18 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
+  componentDidMount() {
+    const script = document.createElement('script');
+    const anchor = document.getElementById('inject-comments-for-uterances');
+    script.setAttribute('src', 'https://utteranc.es/client.js');
+    script.setAttribute('crossorigin', 'anonymous');
+    script.setAttribute('async', true);
+    script.setAttribute('repo', '[ENTER REPO HERE]');
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('theme', 'github-light');
+    anchor.appendChild(script);
+  }
+
   render(): JSX.Element {
     return (
       <Html>
@@ -10,12 +22,21 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+          {/* <script
+            src="https://utteranc.es/client.js"
+            repo="andersonsts/challenge1-chapter3-reactjs"
+            issue-term="pathname"
+            theme="github-dark"
+            crossOrigin="anonymous"
+            async
+          /> */}
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
           <NextScript />
+          <div id="inject-comments-for-uterances" />
         </body>
       </Html>
     );
